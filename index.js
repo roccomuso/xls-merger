f3ea69525b061ef81ae9335fe9889825e7bc427d
rocco <rocco@hackerstribe.com>
2017-04-01 18:09:54 +0200

    stable version

--- index.js
+++ index.js
@@ -25,44 +25,41 @@ var headers = {}
 
 for (var sheetName in files){
   var sheet = files[sheetName]
-  console.log(sheet.length)
   // sheet[row][col]
+  //console.log(sheet)
   var header = _.flatten(_.pullAt(sheet, 0))
   headers[sheetName] = header
   oIndex = header.indexOf(oColumn)
   /* sheet has no header now */
   rawSheet = rawSheet.concat(sheet)
   /* loop the rows */
-  //sheet.forEach(function(row, idx) { console.log(idx, row); })
+  //rawSheet.forEach(function(row, idx) { console.log(idx, row); })
 }
 
 /* check wether headers are equal */
 var header = null;
-debugger
+
 for (var sheetName in headers){
   header = header || headers[sheetName]
-  debugger
   if (!_.isEqual(headers[sheetName], header)) error('Files headers are not equal. Remember, Headers are case-sensitive!')
 }
 
 /* check if the column exists */
 if (oIndex === -1) error('Column not found. NB. the field is case-sensitive')
 
-console.log(oIndex)
-console.log(rawSheet.length)
-
 /* order by the given Column */
 var orderedSheet = _.sortBy(rawSheet, function(row){
   if (isDate(row[oIndex]))
-  return new Date(row[oIndex])
-  else row[oIndex]
+    return new Date(row[oIndex])
+  else return row[oIndex]
 })
 
-console.log(orderedSheet.length)
+//console.log(orderedSheet)
 
 /* add the header to the orderedSheet */
 orderedSheet.unshift(header)
 
-console.log(orderedSheet.length)
-// TODO: write to file
-// writeXls(...)
+
+/* write to file */
+console.log('Writing to file', orderedSheet.length, 'rows')
+writeXls(orderedSheet, 'output.xls')
